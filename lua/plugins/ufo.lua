0d406878595c5017a020dbdf2c4ef11a6805fabe
moeezali2375 <moeezali2375@gmail.com>
2025-07-03 13:44:00 +0500

    Add Ufo for code folding

---
+++ lua/plugins/ufo.lua
@@ -0,0 +1,61 @@
+return {
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = { 'kevinhwang91/promise-async' },
+    config = function()
+      vim.o.foldcolumn = '0'
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      local ufo = require 'ufo'
+
+      vim.keymap.set('n', 'zR', ufo.openAllFolds, { desc = 'Open all folds' })
+      vim.keymap.set('n', 'zM', ufo.closeAllFolds, { desc = 'Close all folds' })
+      vim.keymap.set('n', 'zK', function()
+        local winid = ufo.peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end, { desc = 'Peek folded lines' })
+
+      vim.api.nvim_set_hl(0, 'FoldSuffix', { fg = '#23e449', bg = '#1F2335' })
+      vim.api.nvim_set_hl(0, 'Folded', { fg = '#ffffff', bg = '#042632' })
+
+      local handler = function(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+          local chunkText = chunk[1]
+          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+          else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            if curWidth + chunkWidth < targetWidth then
+              suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+          end
+          curWidth = curWidth + chunkWidth
+        end
+        table.insert(newVirtText, { suffix, 'FoldSuffix' })
+        return newVirtText
+      end
+
+      require('ufo').setup {
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'lsp', 'indent' }
+        end,
+        open_fold_hl_timeout = 0,
+        fold_virt_text_handler = handler,
+      }
+    end,
+  },
+}
