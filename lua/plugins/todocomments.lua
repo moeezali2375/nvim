9b3181e5369f40f3ee64269f8464ea49485331c7
moeezali2375 <moeezali2375@gmail.com>
2025-07-09 11:49:46 +0500

    Add keymaps for TODOS

--- lua/plugins/todocomments.lua
+++ lua/plugins/todocomments.lua
@@ -1,1 +1,27 @@
-return { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
+return {
+  'folke/todo-comments.nvim',
+  event = 'VimEnter',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    signs = false,
+  },
+  config = function(_, opts)
+    require('todo-comments').setup(opts)
+
+    local keymap = vim.keymap.set
+    local silent = { silent = true }
+
+    -- 👈 Jump to previous TODO
+    keymap('n', ']t', function()
+      require('todo-comments').jump_next()
+    end, { desc = 'Next todo comment' })
+
+    -- 👉 Jump to next TODO
+    keymap('n', '[t', function()
+      require('todo-comments').jump_prev()
+    end, { desc = 'Previous todo comment' })
+
+    -- 🔭 Telescope search for TODOs
+    keymap('n', '<leader>st', ':TodoTelescope<CR>', { desc = 'Search todos with Telescope' })
+  end,
+}
